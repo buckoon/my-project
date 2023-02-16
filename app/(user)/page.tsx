5622233ae13f9bb39774467e181535a361d5320e
buckoon <jeremy8@vt.edu>
2023-02-16 16:25:26 -0500

    stuck on groq request

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,8 +1,29 @@
-export default function HomePage() {
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client"
+
+const query = groq`
+    *[_type=='post'] {
+        ...,
+        author->,
+        categories[]->
+    } | order(_createdAt desc)
+`;
+
+export default async function HomePage() {
+    if (previewData()) {
+        return<div>Preview mode</div>;
+
+    }
+
+    const posts = await client.fetch(query);
+    console.log(posts);
+    
+
     return (
         <div>
-            <h1 className="text-4xl"> Welcome to the blog</h1>
-            <p>Some Content</p>
+            <h1>Not in preview mode</h1>
+           
         </div>
     );
 }
